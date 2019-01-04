ac6ed702bbfbd301e9f397f3b2972777e5dde165
Robin-Sb <robin.schwab@hs-furtwangen.de>
2019-01-04 17:44:41 +0100

    initial generic tree

---
+++ tree/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace tree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tree = new Tree<String>();
+            var root = tree.CreateNode("root");
+            var child1 = tree.CreateNode("child1");
+            var child2 = tree.CreateNode("child2");
+
+            root.AppendChild(child1);
+            root.AppendChild(child2);
+
+            var grand11 = tree.CreateNode("grand11");
+            var grand12 = tree.CreateNode("grand12");
+            var grand13 = tree.CreateNode("grand13");
+
+            child1.AppendChild(grand11);
+            child1.AppendChild(grand12);
+            child1.AppendChild(grand13);
+
+            var grand21 = tree.CreateNode("grand21");
+
+            child2.AppendChild(grand21);
+            child1.RemoveChild(grand12);
+
+            root.PrintTree(0);
+            root.PrintTree(0);
+        }
+    }
+}
---
+++ tree/Tree.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+
+namespace tree
+{
+    public class Tree <T>
+    {
+        public Tree() {}
+        public Tree (T type) {
+            this.type = type;
+        }
+        public T type;
+        public T parent;
+        public List<Tree<T>> children = new List<Tree<T>>();
+        public static bool calledFirstTime = true; 
+        public Tree<T> CreateNode(T element) 
+        {
+            Tree<T> node = new Tree<T>(element);
+            return node;
+        }
+
+        public void AppendChild(Tree<T> child)
+        {
+            child.parent = this.type;
+            children.Add(child);    
+        }
+        public void RemoveChild(Tree<T> child)
+        {
+            children.Remove(child);
+        }
+
+        public void PrintTree(int generation) 
+        {
+            if (calledFirstTime)
+            {
+                Console.WriteLine(this.ToString());
+            }
+
+            calledFirstTime = false;
+            generation++;
+            for (int i = 0; i < children.Count; i++)
+            {
+                Console.WriteLine(ToStars(generation) + children[i].ToString());
+                children[i].PrintTree(generation);
+            }
+        }
+        
+        public override string ToString() 
+        {
+            return this.type.ToString();
+        }
+
+        private static string ToStars(int generation)
+        {
+            string stars = "";
+            for (int i = 0; i < generation; i++)
+            {
+                stars += "*";
+            }
+            return stars;
+        }
+    }
+}
